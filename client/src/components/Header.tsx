ca0e907decb197015077199cc8f2106c5be11209
Connie Chin <conniec96@gmail.com>
2025-02-07 03:47:19 +0000

    Worked on css, need to firgure out how to move icons responsively.

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,23 +1,30 @@
 import './Header.css';
-import { HiOutlineSearch } from 'react-icons/hi';
+import { HiOutlineSearch, HiOutlineMenu } from 'react-icons/hi';
 import { HiOutlineUser, HiOutlineShoppingBag } from 'react-icons/hi2';
 
 export function Header() {
   return (
     <>
-      <div className="flex max-w-full border-2 justify-between items-center p-7">
-        <div className="text-2xl w-[78px] border-2 border-red-400">
-          <HiOutlineSearch />
-        </div>
-        <h1 className="text-4xl text-orange-400 md:flex-1 md: text-left text-center border-2 border-red-400">
+      <div className="flex max-w-full border-2 p-7 justify-between md:justify-center md:items-center">
+        <h1 className="text-4xl text-orange-400 md:text-center md:w-full">
           Eternal Daze
         </h1>
-        <div className="text-2xl flex w-[78px] justify-end gap-4 md:ml-auto border-2 border-red-400">
-          <HiOutlineUser />
-          <HiOutlineShoppingBag />
+        <div className="flex justify-evenly items-center md:ml-auto">
+          <div className="text-2xl w-[30px] flex justify-center md:ml-auto">
+            <HiOutlineSearch />
+          </div>
+          <div className="text-2xl w-[30px] flex justify-center md:ml-auto">
+            <HiOutlineUser />
+          </div>
+          <div className="text-2xl w-[30px] flex justify-center md:ml-auto">
+            <HiOutlineShoppingBag />
+          </div>
+          <div className=" text-2xl w-[30px] flex justify-center md:hidden">
+            <HiOutlineMenu />
+          </div>
         </div>
       </div>
-      <div className="flex justify-center text-[15px]">
+      <div className="flex justify-center text-[15px] hidden md:flex">
         <p className="m-[20px]">Shop All</p>
         <p className="m-[20px]">Shirts</p>
         <p className="m-[20px]">Pants</p>
